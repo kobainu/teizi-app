bf88a67e306372399c0360263912f8c3f8877220
kobainu <kobadai0520.1@gmail.com>
2023-01-15 09:22:21 +0900

    フロントエンドとのAPI接続テスト

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { execTest } from "./lib/api/test";
 
 export const App = () => {
-  return <h1>Hello World!</h1>;
+  const [message, setMessage] = useState("");
+
+  const handleExecTest = async () => {
+    const res = await execTest();
+
+    if (res.status === 200) {
+      setMessage(res.data.message);
+    }
+  };
+
+  useEffect(() => {
+    handleExecTest();
+  }, []);
+
+  return <h1>{message}</h1>;
 };
---
+++ frontend/src/lib/api/client.js
@@ -0,0 +1,14 @@
+import applyCaseMiddleware from "axios-case-converter";
+import axios from "axios";
+
+// ヘッダーに関してはケバブケースのままで良いので適用を無視するオプションを追加
+const options = {
+  ignoreHeaders: true,
+};
+
+export const client = applyCaseMiddleware(
+  axios.create({
+    baseURL: "http://localhost:3001/api/v1",
+  }),
+  options
+);
